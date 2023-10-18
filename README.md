5691db84ca7647b667b73b39e10931f74de4becd
Anse98 <lorenzo.anselmi98.com>
2023-10-18 17:07:17 +0200

    ADD-Milestone 2

--- README.md
+++ README.md
@@ -3,8 +3,14 @@
 
 ## Creo l’array di oggetti con le informazioni fornite
 
-## StampO su console, per ogni membro del team, le informazioni di nome, ruolo e la     stringa della foto
+## Stampo su console, per ogni membro del team, le informazioni di nome, ruolo e la     stringa della foto
 
   - Ciclo for per andare a prendere ogni oggetto dell'array e salvarmi i valori delle proprietà in una variabile
 
   - Stampo in console il risultato
+
+## Invece che stampare su console come nello step precedente, stampo nell'html
+
+  - Vado a prendermi dal DOM il container in cui poi concatenerò le stringhe 
+
+  - Vado a concatenare col template literal la mia stringa tot volte, che contiene le     varie classi e i valori delle proprietà degli oggetti, dentro al container 
--- app.js
+++ app.js
@@ -3,61 +3,76 @@
 //Creo l’array di oggetti con le informazioni fornite
 const teamMembers = [
   {
-    nome : 'Wayne Barnett',
+    nome: 'Wayne Barnett',
     ruolo: 'Founder & CEO',
-    foto : 'wayne-barnett-founder-ceo.jpg',
+    foto: 'wayne-barnett-founder-ceo.jpg',
   },
 
   {
-    nome : 'Angela Caroll',
+    nome: 'Angela Caroll',
     ruolo: 'Chief Editor',
     foto: 'angela-caroll-chief-editor.jpg'
   },
 
   {
-    nome : 'Walter Gordon',
+    nome: 'Walter Gordon',
     ruolo: 'Office Manager',
     foto: 'walter-gordon-office-manager.jpg'
   },
 
   {
-    nome : 'Angela Lopez',
+    nome: 'Angela Lopez',
     ruolo: 'Social Media Manager',
     foto: 'angela-lopez-social-media-manager.jpg'
   },
 
   {
-    nome : 'Scott Estrada',
+    nome: 'Scott Estrada',
     ruolo: 'Developer',
     foto: 'scott-estrada-developer.jpg'
   },
 
   {
-    nome : 'Barbara Ramos',
+    nome: 'Barbara Ramos',
     ruolo: 'Graphic Designer',
     foto: 'barbara-ramos-graphic-designer.jpg'
   }
 ]
 
-const outputDOMElement = document.getElementById("output");
 
-
-// MILESTONE 1
+// MILESTONE 1 E MILESTONE 2
 
 
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 
 // Ciclo for per andare a prendere ogni oggetto dell'array e salvarmi i valori delle proprietà in una variabile
-for(let i = 0; i < teamMembers.length; i++) {
-  const name = teamMembers[i].nome;
-  console.log(name);
-  const role = teamMembers[i].ruolo;
-  console.log(role);
-  const photo = teamMembers[i].foto;
-  console.log(photo);
+for (let i = 0; i < teamMembers.length; i++) {
+
+  const member = teamMembers[i];
+
+  const name = member.nome;
+
+  const role = member.ruolo;
+
+  const photo = member.foto;
+
+  // Vado a prendermi dal DOM il container in cui poi concatenerò le stringhe 
+  const containerDOMElement = document.querySelector(".container");
+
+  // Vado a concatenare col template literal la mia stringa tot volte, che contiene le varie classi e i valori delle proprietà degli oggetti, dentro al container 
+  containerDOMElement.innerHTML += `<div class="card-container">
+   <div class="member-card p-2">
+     <figure>
+       ${photo}
+     </figure>
+     <p>${name}</p>
+     <p>${role}</p>
+   </div>
+  </div>`;
 }
 
-// MILESTONE 2
+
+
 
 
 
